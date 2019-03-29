f0f71b6a89b2f44a2994947bbe9dcf8d3c242282
Allen Wyma <allen@plangora.com>
2019-03-29 03:54:25 -0700

    switch back to using socket state

--- lib/gigalixir_phoenix_web/channels/room_channel.ex
+++ lib/gigalixir_phoenix_web/channels/room_channel.ex
@@ -4,13 +4,17 @@ defmodule GigalixirPhoenixWeb.RoomChannel do
 
   def join(_, _params, socket) do
     send(self(), :counter)
-    {:ok, assign(socket, :timer, 0)}
+    socket =
+      socket
+      |> assign(:greeting, @greeting)
+      |> assign(:timer, 0)
+    {:ok, socket}
   end
 
-  def handle_info(:counter, %{assigns: %{timer: timer}} = socket) do
+  def handle_info(:counter, %{assigns: %{timer: timer, greeting: greeting}} = socket) do
     Process.send_after(self(), :counter, 1000)
     timer = timer + 1
-    push(socket, "count", %{timer: timer, greeting: @greeting})
+    push(socket, "count", %{timer: timer, greeting: greeting})
     {:noreply, assign(socket, :timer, timer)}
   end
 end
